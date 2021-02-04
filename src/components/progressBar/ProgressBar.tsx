75b7d760f18cddf9f3bcff3966d4c4cd3b3be337
Hyungjun Kim <hyungjun.dev@gmail.com>
2021-02-04 21:31:31 +0100

    add progress bar animation

--- src/components/progressBar/ProgressBar.tsx
+++ src/components/progressBar/ProgressBar.tsx
@@ -1,17 +1,27 @@
-import React, { FunctionComponent } from 'react';
+import React, { useState, useEffect, FunctionComponent } from 'react';
 
 type ProgressBarType = {
     step: number,
     totalSteps: number
 }
 
+function getProgressByStep(step: number, totalSteps: number) {
+    return Math.floor(step / totalSteps * 100) / 100;
+}
+
 const ProgressBar: FunctionComponent<ProgressBarType> = (props) => {
+    const [progress, setProgress] = useState(getProgressByStep(props.step - 1, props.totalSteps));
+
+    useEffect(() => {
+        setProgress(getProgressByStep(props.step, props.totalSteps));
+    })
+
     return (
         <div className="relative pt-4">
             <div className="overflow-hidden h-1 mb-4 text-xs flex bg-purple-100">
                 <div 
-                    style={{width: `${Math.floor(props.step / props.totalSteps * 100)}%`}} 
-                    className="bg-purple-700">
+                    style={{ transform: `scaleX(${progress})` }} 
+                    className="w-full transition-transform origin-left duration-300 ease-in-out bg-purple-700">
                 </div>
             </div>
         </div>
